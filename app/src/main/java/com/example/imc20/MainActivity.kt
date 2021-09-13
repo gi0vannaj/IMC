05588caed7a02ca83c58c5808632b6effa33e9dc
GIOVANNA JOVINA CORREIA DOS SANTOS <21193379@CFP127ALUNO.LOCAL>
2021-09-13 16:44:20 -0300

    programaçao calculo imc

--- app/src/main/java/com/example/imc20/MainActivity.kt
+++ app/src/main/java/com/example/imc20/MainActivity.kt
@@ -4,19 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editPeso = findViewById<EditText>(R.id.edit_peso)
-        val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+        //declaraçao de variavel
+        //var x = 10 criei e inicializei e posso mudar, assim criou-se uma constante
+        //val x= 10 criei, inicializei mas nao posso mudar o valor pois assim é uma constante
 
+        //crinado as instancias das View para que possam ser manipuladas em kotlin
+        val editTextPeso = findViewById<EditText>(R.id.edit_peso)
+        val editTextAltura = findViewById<EditText>(R.id.edit_altura)
+        val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+        val textViewResultado = findViewById<TextView>(R.id.text_view_resultado)
+
+        //adicionando evento de um ouvinte de click no botao
         buttonCalcular.setOnClickListener{
-            Toast.makeText(this, editPeso.toString())
+            if (editTextPeso.text.isEmpty()){
+                editTextPeso.error = "peso é obrigatorio"
+            }else if(editTextAltura.text.isEmpty()){
+                editTextAltura.error = "altura é obrigatorio"
+            }else{
+                val peso = editTextPeso.text.toString().toInt()
+                val altura = editTextAltura.text.toString().toDouble()
+
+                val imc = peso/(altura* altura)
+
+                textViewResultado.text =String.format("%.1f",imc)
+            }
+
+        }
+
+
         }
 
     }
-}
